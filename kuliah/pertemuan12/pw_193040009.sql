98435b06f4de01b351f1448a411c1a85405d112b
Muhammad Wildhan <muhammadwildan334@gmail.com>
2020-05-06 12:35:43 +0700

    Kuliah Pertemuan 12

---
+++ kuliah/pertemuan12/pw_193040009.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 06 Mei 2020 pada 07.30
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw_193040009`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nrp` char(9) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `jurusan` varchar(100) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
+(1, 'Muhammad Wildhan', '193040009', '193040009.muhammad@mail.unpas.ac.id', 'Teknik Informatika', 'wildhan.jpg'),
+(2, 'Sofyan Egi', '193040025', '193040025.sofyan@mail.unpas.ac.id', 'Teknik Informatika', 'egi.jpg'),
+(3, 'Farhan A', '193040010', '193040010.farhan@mail.unpas.ac.id', 'Teknik Informatika', 'farhan.jpg'),
+(4, 'Putra Hanif Ammarta', '193040001', '193040001.putra@mail.unpas.ac.id', 'Teknik Informatika', 'hanif.jpg'),
+(5, 'David Dalil Tauhid', '193040002', '193040002.david@mail.unpas.ac.id', 'Teknik Informatika', 'david.jpg'),
+(6, 'Renal Mutaqin', '193040037', '193040037.renali@mail.unpas.ac.id', 'Teknik Informatika', 'renal.jpg'),
+(7, 'Taufik Hidayat', '193040019', '193040019.taufik@mail.unpas.ac.id', 'Teknik Informatika', 'taufik.jpg'),
+(8, 'Silvi Fitriawati', '193040028', '193040028.silvi@@mail.unpas.ac.id', 'Teknik Informatika', 'silvi.jpg'),
+(9, 'Anjara Darojatun Nisa', '193040006', '193040006.anjara@mail.unpas.ac.id', 'Teknik Informatika', 'anjara.jpg'),
+(10, 'Fahri Arliansyah', '193040011', '193040011.fahri@mail.unpas.ac.id', 'Teknik Informatika', 'fahri.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$i.vuw1jCJI.4xLDRo27ieeyFyQJFbm9jeo2QNe2H29KEykMFoksIG');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ kuliah/pertemuan12/ubah.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+// kalo gaada id
+if (!isset($_GET['id'])) {
+  header(
+    "Location: index.php"
+  );
+  exit;
+}
+
+// ambil id dari url
+$id = $_GET['id'];
+
+//query mahasiswa berdasarkan id 
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+// cek apakah tombol tambah sudah ditekkan
+
+if (isset($_POST['ubah'])) {
+  if (ubah($_POST) > 0) {
+    echo "<script>
+    alert('data berhasil diubah!');
+    document.location.href='index.php';
+    </script>";
+  } else {
+    echo "data gagal diubah!";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ubah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form Ubah Data mahasiswa</h3>
+  <form action="" method="POST">
+    <input type="hidden" name="id" value="<?= $m['id'] ?>">
+    <ul>
+      <li>
+        <label>
+          Nama :
+          <input type="text" name="nama" autofocus required value="<?= $m['nama'] ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          NRP :
+          <input type="text" name="nrp" required value="<?= $m['nrp'] ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Email :
+          <input type="text" name="email" required value="<?= $m['email'] ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Jurusan :
+          <input type="text" name="jurusan" required value="<?= $m['jurusan'] ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Gambar :
+          <input type="text" name="gambar" required value="<?= $m['gambar'] ?>">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="ubah"> Ubah Data!</button>
+      </li>
+    </ul>
+
+
+  </form>
+</body>
+
+</html>
