327000f75f35f1c8f05595487f32391fd778815a
Muhammad Wildhan <muhammadwildan334@gmail.com>
2020-05-17 22:37:02 +0700

    tubes

---
+++ index.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="style.css">
+
+  <title>Muhammad Wildhan</title>
+</head>
+
+<body>
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Muhammad Wildhan</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="#home">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link active" href="#profile">Profile</a>
+          <a class="nav-item nav-link active" href="#kuliah">Kuliah</a>
+          <a class="nav-item nav-link active" href="#praktikum">Praktikum</a>
+          <a class="nav-item nav-link active" href="#tubes">Tugas Besar</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!--Akhir Navbar-->
+
+  <!--Jumbotron-->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Selamat Datang di Website Wildhan</h1>
+      <p class="lead">Website ini berisi Profile pribadi saya dan tugas - tugas Mata kuliah Pemrograman Web.</p>
+    </div>
+  </div>
+  <!--Akhir Jumbotron-->
+
+  <!--Profile-->
+  <section id="profile" class="profile scrollspy">
+    <div class="container">
+      <div class="row">
+        <h3 class="container text-center  justify-content-center">Profile</h3>
+        <div class="col m2 light">
+          <div class="profile">
+            <h5>Biodata Diri</h5>
+            <p>Perkenalkan nama saya Muhammad Wildhan Kusumawardana.
+              Teman-teman saya biasa memanggil saya Wildan.
+              Saya Lahir di Tasikmalaya pada tanggal 09 Agustus 2001.
+              Saya besar dan tinggal di Kota Tangerang Selatan, Banten.
+              Hobi saya yaitu Bermain Futsal,Badminton,Games DLL.
+              Saya Sekolah Dasar di SDN Suradita di daerah Cisauk, Tangerang.
+              Lalu saya melanjutkan Sekolah saya di MTS AN-NAJAH Bogor.
+              Setelah itu,Saya melanjutkan pendidikan saya di SMK Letris Indonesia 2.
+              Lalu setelah saya lulus dari SMK, Saya melanjutkan pendidikan saya di Perkuliahan Universitas Pasundan Bandung Jawa Barat.
+              Di Universitas Pasundan saya memilih masuk Jurusan Teknik Informatika
+              Alasan saya memilih Jurusan tersebut karena di SMK saya masuk Jurusan RPL,
+              jadi saya ingin melanjutkan apa yang sudah saya pelajari dan mempelajari lebih dalam tentang Pemrograman.</p>
+          </div>
+        </div>
+        <div class="col m2 light">
+          <div class="img-profile">
+            <img src="image/profile.jpg" style="width: 500px; margin-top: 50px" alt="Foto Profile" class="responsive-img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><br>
+  <!--Akhir Profile-->
+
+  <!--Kuliah-->
+  <div class="kuliah scrollspy pt-5 toping" id="kuliah">
+    <section>
+      <div class="container tools scrollspy pt-6">
+        <div class="row pl-2 justify-content-center text-center">
+          <div class="card bg-light mb-6 col-lg-6">
+            <div class="card-header "><br>
+              <h4 class="card-title">Tugas Kuliah</h4>
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item"><a href="kuliah/pertemuan9/" class="btn btn-primary btn-block">Pertemuan 9</a></li>
+              <li class="list-group-item"><a href="kuliah/Pertemuan10/" class="btn btn-primary btn-block">Pertemuan 10</a></li>
+              <li class="list-group-item"><a href="kuliah/Pertemuan11/" class="btn btn-primary btn-block">Pertemuan 11</a></li>
+              <li class="list-group-item"><a href="kuliah/Pertemuan12/" class="btn btn-primary btn-block">Pertemuan 12</a></li>
+              <li class="list-group-item"><a href="kuliah/Pertemuan13/" class="btn btn-primary btn-block">Pertemuan 13</a></li>
+            </ul>
+          </div>
+          <div class="col m2 light">
+            <div class="img-profile">
+              <img src="image/img1.png" style="width: 530px; height: 350px; margin-top: 60px" alt="Foto Profile" class="responsive-img">
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </section><br>
+  <!--Akhir Kuliah-->
+
+  <!--Praktikum-->
+  <div class="praktikum scrollspy pt-5 toping" id="praktikum">
+    <section>
+      <div class="container tools scrollspy pt-6">
+        <div class="row pl-2 justify-content-center text-center">
+          <div class="col m2 light">
+            <div class="img-profile">
+              <img src="image/img2.png" style="width: 530px; height: 350px; margin-top: 85px" alt="Foto Profile" class="responsive-img">
+            </div>
+          </div>
+          <div class="card bg-light mb-6 col-lg-6">
+            <div class="card-header "><br>
+              <h4 class="card-title">Tugas Praktikum</h4>
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item"><a href="praktikum/P1_SENIN13_193040009" class="btn btn-primary btn-block">P1_SENIN13_193040009</a></li>
+              <li class="list-group-item"><a href="praktikum/P2_SENIN13_193040009" class="btn btn-primary btn-block">P2_SENIN13_193040009</a></li>
+              <li class="list-group-item"><a href="praktikum/P3_SENIN13_193040009" class="btn btn-primary btn-block">P3_SENIN13_193040009</a></li>
+              <li class="list-group-item"><a href="praktikum/P4_SENIN13_193040009" class="btn btn-primary btn-block">P4_SENIN13_193040009</a></li>
+              <li class="list-group-item"><a href="praktikum/P5_SENIN13_193040009" class="btn btn-primary btn-block">P5_SENIN13_193040009</a></li>
+              <li class="list-group-item"><a href="praktikum/P6_SENIN13_193040009" class="btn btn-primary btn-block">P6_SENIN13_193040009</a></li>
+              <li class="list-group-item"><a href="praktikum/P7_SENIN13_193040009" class="btn btn-primary btn-block">P7_SENIN13_193040009</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  </div>
+  </section><br>
+  <!--Akhir Praktikum-->
+
+  <!--Tugas Besar-->
+  <div class="tubes scrollspy pt-5 toping" id="tubes">
+    <section>
+      <div class="container tools scrollspy pt-6">
+        <div class="row pl-2 justify-content-center text-center">
+          <div class="card bg-light mb-6 col-lg-6">
+            <div class="card-header "><br>
+              <h4 class="card-title">Tugas Besar</h4>
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item"><a href="tubes/index.php" class="btn btn-primary btn-block">Tugas Besar</a></li>
+            </ul>
+          </div>
+          <div class="col m2 light">
+            <div class="img-profile">
+              <img src="image/img3.png" style="width: 530px; height: 350px; margin-top: 60px" alt="Foto Profile" class="responsive-img">
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </section><br><br>
+  <!--Akhir Tugas Besar-->
+
+  <!--Footer-->
+  <footer class="pt-3 pb-2">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 col-sm-12">
+          <p>&copy; 2020 Copyright<a href=""> Muhammad Wildhan</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--Akhir Footer-->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
